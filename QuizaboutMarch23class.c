b960a75a7a30a42285fcb340bd8bd802ce6c2bec
Norayr Ashughyan <99163306+Noro7@users.noreply.github.com>
2022-03-26 21:48:02 +0400

    Add files via upload

---
+++ QuizaboutMarch23class.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void set_nth_1(int*q, int n)
+{
+	int shift = 1 <<(n-1);
+	*q= *q|shift;};
+void set_nth_0(int*q,int n)
+{
+	int shift = 1 <<(n-1);
+	*q=*q & (!(shift));
+};
+void toggle_nth(int*q,int n)
+{
+	*q=!*q;
+}
+int main()
+{
+	int M = 1801;
+	int N = 7;
+	set_nth_1(&M,N);
+	printf("%d",M);
+	return 0;
+};
